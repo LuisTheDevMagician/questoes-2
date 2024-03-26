4d59eacd4e7d73e6c93493093fa4fcec8cd604f6
LuisTheDevMagician <164002219+LuisTheDevMagician@users.noreply.github.com>
2024-03-26 11:27:53 -0300

    Update questao2-capitulo4.c

--- questao2-capitulo4.c
+++ questao2-capitulo4.c
@@ -14,16 +14,15 @@ int main(){
 	printf("Informe a segunda nota: \n");
 	scanf("%f", &n2);
 	
+	soma=(n1+n2)/2;
 	
-	if((n1+n2/2) >= 0 && (n1+n2)/2<3){
-	printf("Reprovado \n");
-	}else if ((n1+n2)/2 >= 3 && (n1+n2)/2<7){ 
-	printf("Exame \n");
-    }else{ 
-    printf("Aprovado \n");
-
+        if(soma>=0 && soma<3){
+		printf("Reprovado\n");
+	}else if(soma>=3 && soma<7){
+		printf("Exame\n");
+	}else if(soma>=7 && soma<=10){
+		printf("Aprovado\n");
 	}
-
 	
     printf("o programa vai fechar em 5 segundos...");
     sleep(5);
